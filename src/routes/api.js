4b65cd81c7da6ad793fc4f9f9d07318105b238b0
namdoanx03 <dpnbro1102@gmail.com>
2024-06-06 03:42:14 +0700

    vid 58

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,7 +1,7 @@
 import express from "express";
 import apiController from '../controller/apiController'
 import userController from '../controller/userController'
-
+import groupController from '../controller/groupController'
 const router = express.Router()
 
 const initApiRoutes = (app) => {
@@ -17,6 +17,7 @@ const initApiRoutes = (app) => {
     router.put("/user/update", userController.updateFunc)
     router.delete("/user/delete", userController.deleteFunc)
 
+    router.get("/group/read", groupController.readFunc)
     return app.use("/api/v1/", router)
 }
 export default initApiRoutes
