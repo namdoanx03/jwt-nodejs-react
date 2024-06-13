edf763c0179b748f542f8e7e11b957e9d914e9ca
namdoanx03 <dpnbro1102@gmail.com>
2024-06-14 03:55:36 +0700

    vid 72

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -82,6 +82,18 @@ const deleteFunc = async(req, res) => {
         })
     }
 }
+const getUserAccount = async( req, res) => {
+    return res.status(200).json({
+        EM: 'ok',
+        EC: 0,
+        DT: {
+            access_token: req.token,
+            groupWithRoles: req.user.groupWithRoles,
+            email: req.user.email,
+            username: req.user.username
+        }
+    })
+}
 module.exports = {
-    readFunc, createFunc, updateFunc, deleteFunc
+    readFunc, createFunc, updateFunc, deleteFunc, getUserAccount
 }
